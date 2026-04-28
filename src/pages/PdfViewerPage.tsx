4f5a20f3ef4ad8d4610c1e9b1a2d3363178b74ef
dthofficialmusic-boop <dthofficialmusic@gmail.com>
2026-04-27 19:52:22 -0500

    feat: Update PDF viewer to use new artist services doc
    Replaces the old PDF document URL in the PDF viewer with the newly uploaded version for artist services.

--- src/pages/PdfViewerPage.tsx
+++ src/pages/PdfViewerPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const PDF_URL = "https://storage.googleapis.com/forcotidorrecords/Cotidor_Records_Artist_Services_Understanding.pdf";
+const PDF_URL = "https://storage.googleapis.com/forcotidorrecords/(NEW)Cotidor_Records_Artist_Services_Understanding.pdf";
 const GOOGLE_VIEWER_URL = `https://docs.google.com/gview?url=${encodeURIComponent(PDF_URL)}&embedded=true`;
 
 export default function PdfViewerPage() {
